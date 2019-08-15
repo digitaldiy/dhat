17119d490c7b794f3c1b1fd1aea116defbc96513
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-08-15 19:41:08 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -44,13 +44,13 @@ export class Navigation extends Component {
 
     return (
       <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
-        <div className="Nav--Container alignment-right container">
+        <div className="Nav--Container  container">
           <Link to="/" onClick={this.handleLinkClick}>
             <Logo />
           </Link>
-          <NavLink to="/blog/">Findings</NavLink>
+          <NavLink className="navbar-end" to="/blog/">Findings</NavLink>
           
-            </div>
+          
          
           <button
             className="Button-blank Nav--MenuButton"
@@ -58,7 +58,7 @@ export class Navigation extends Component {
           >
             {active ? <X /> : <Menu />}
           </button>
-      
+      </div>
       </nav>
     )
   }
